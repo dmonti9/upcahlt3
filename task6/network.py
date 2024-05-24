dc0f5412067f0a3628af5906b5ae86945330c2f7
Karl <karl-jahnel@web.de>
2024-05-24 22:17:59 +0200

    bestertry

--- task6/network.py
+++ task6/network.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from sklearn.decomposition import PCA
+
 criterion = nn.CrossEntropyLoss()
+
 
 class ddiCNN(nn.Module):
     def __init__(self, codes):
@@ -21,40 +24,47 @@ class ddiCNN(nn.Module):
         emb_dim_lemmas = 50
 
         # LSTM configuration
-        hidden_dim = 128
         num_layers = 2
         bidirectional = True
-
+        total_dim = emb_dim_word + emb_dim_pos + emb_dim_lc + emb_dim_lemmas
+        lstm_output_dim = total_dim if bidirectional else total_dim // 2
+        
         # Create layers
         self.embW = nn.Embedding(n_words, emb_dim_word, padding_idx=0)
         self.embPOS = nn.Embedding(n_pos, emb_dim_pos, padding_idx=0)
         self.embLC = nn.Embedding(n_lc_word, emb_dim_lc, padding_idx=0)
         self.embLemmas = nn.Embedding(n_lemmas, emb_dim_lemmas, padding_idx=0)
-        self.lstm = nn.LSTM(emb_dim_word + emb_dim_pos+emb_dim_lc+emb_dim_lemmas, hidden_dim, num_layers,
+        self.lstm = nn.LSTM(total_dim, total_dim // 2, num_layers,
                             batch_first=True, bidirectional=bidirectional, dropout=0.5)
         
-        # CNN layer with Batch Normalization
-        lstm_output_dim = hidden_dim * 2 if bidirectional else hidden_dim
-        self.cnn = nn.Conv1d(lstm_output_dim, 32, kernel_size=3, stride=1, padding='same')
-        self.pool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
-        self.out = nn.Linear(32 * (max_len // 2), n_labels)  # Adjusted for max pooling
+        # CNN layers with Batch Normalization
+        self.conv1 = nn.Conv1d(lstm_output_dim, 32, kernel_size=3, stride=1, padding='same')
+        self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+        
+        self.conv2 = nn.Conv1d(32, 64, kernel_size=3, stride=1, padding='same')
+        self.pool2 = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+        
+        self.out = nn.Linear(64 * (max_len // 4), n_labels)  # Adjusted for max pooling
 
-
-    def forward(self, w, p,lc,lemmas):
+    def forward(self, w, p, lc, lemmas):
         x = self.embW(w)
         y = self.embPOS(p)
         u = self.embLC(lc)
         v = self.embLemmas(lemmas)
-        x = torch.cat((x, y,u,v), dim=2)
+        x = torch.cat((x, y, u, v), dim=2)
 
         x, _ = self.lstm(x)
         x = x.permute(0, 2, 1)
 
-        x = self.cnn(x)
+        x = self.conv1(x)
         x = F.relu(x)
-        x = self.pool(x)  # Apply max pooling
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
 
         x = x.flatten(start_dim=1)
-
         x = self.out(x)
+        
         return x
