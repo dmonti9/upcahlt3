fb7a39a1b7364a5b3ac7e068e2bfe4c41e6379fe
Karl <karl-jahnel@web.de>
2024-05-30 17:25:33 +0200

    aufräumen

--- task5/network.py
+++ task5/network.py
@@ -14,45 +14,31 @@ class nercLSTM(nn.Module):
         n_sufs = codes.get_n_sufs()
         n_lcwords = codes.get_n_lcwords()
         n_labels = codes.get_n_labels()
-
         # Embeddings
-        self.embW = nn.Embedding(n_words, 100)  # Embeddings for words
-        self.embS = nn.Embedding(n_sufs, 50)  # Embeddings for suffixes
-        self.embLW = nn.Embedding(n_lcwords, 50)  # Embeddings for lowercased words
-
-        # Dropout layers
-        self.dropW = nn.Dropout(0.1)
-        self.dropS = nn.Dropout(0.1)
-        self.dropLW = nn.Dropout(0.1)
-
-        # Feature transformation
-        self.feature_transform = nn.Linear(
-            4, 50
-        )  # Transform features to have the same dimensionality
+        emb_words=100
+        emb_sufs=50
+        emb_lw=50
+        emb_features=4
+        total_dim=emb_words+emb_sufs+emb_lw+emb_features
+        self.embW = nn.Embedding(n_words, emb_words)  # Embeddings for words
+        self.embS = nn.Embedding(n_sufs, emb_sufs)  # Embeddings for suffixes
+        self.embLW = nn.Embedding(n_lcwords, emb_lw)  # Embeddings for lowercased words
 
         # LSTM layer
-        self.lstm = nn.LSTM(250, 200, bidirectional=True, batch_first=True)
+        self.lstm = nn.LSTM(total_dim, total_dim//2,2, bidirectional=True, batch_first=True)
 
         # Fully connected layers
-        self.fc1 = nn.Linear(400, 300)  # Process LSTM outputs
-        self.fc2 = nn.Linear(300, n_labels)  # Final output layer
+        self.fc1 = nn.Linear(total_dim, total_dim)  # Process LSTM outputs
+        self.fc2 = nn.Linear(total_dim, n_labels)  # Final output layer
 
     def forward(self, words, suffixes, lcwords, features):
         # Embeddings
+        features=features.float()
         emb_words = self.embW(words)
         emb_sufs = self.embS(suffixes)
         emb_lcwords = self.embLW(lcwords)
-
-        # Apply dropout
-        emb_words = self.dropW(emb_words)
-        emb_sufs = self.dropS(emb_sufs)
-        emb_lcwords = self.dropLW(emb_lcwords)
-
-        # Transform features
-        transformed_features = func.relu(self.feature_transform(features.float()))
-
         # Concatenate all embeddings and transformed features
-        x = torch.cat((emb_words, emb_sufs, emb_lcwords, transformed_features), dim=2)
+        x = torch.cat((emb_words, emb_sufs, emb_lcwords, features), dim=2)
 
         # LSTM processing
         x, _ = self.lstm(x)
